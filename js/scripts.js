dde5cd3039f66626bb6aa3a6eb980a4dae5bce97
alizavr <alina.dmitrievna2407@gmail.com>
2020-10-18 16:05:56 +0300

    add adaptive and animation/ anchor

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,11 @@ $(document).ready(function(){
       }
   });
 });
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
